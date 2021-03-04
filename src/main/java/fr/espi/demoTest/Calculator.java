6fa20bcfd2185e2e57911591d54da4bcbb2acd17
nicolasSAPIN <nicosapin@gmail.com>
2021-03-04 16:44:15 +0100

    re

--- src/main/java/fr/espi/demoTest/Calculator.java
+++ src/main/java/fr/espi/demoTest/Calculator.java
@@ -2,12 +2,25 @@ package fr.espi.demoTest;
 
 public class Calculator {
 	
-		public int add(int a,int b) {
-			return a+b;
-		}
-		public int sub(int a,int b) {
-			return a-b;
-		}
-	
 
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public boolean isPair(int a) {
+        return ((a % 2) == 0);
+    }
 }
+
--- src/test/java/demoTest/Calculatortest.java
+++ src/test/java/demoTest/Calculatortest.java
@@ -3,6 +3,7 @@ package demoTest;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,20 +19,62 @@ public class Calculatortest {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testAdd() {
-		int a = 2;
-		int b = 3;
-		Calculator calc = new Calculator();
-		int actual = calc.add(a, b);
-	}
+    @Test
+    public void testAdd() {
+        int a = 2;
+        int b = 3;
+        Calculator calc = new Calculator();
+        int actual = calc.add(a, b);
+        
+        Assert.assertEquals("Addition fail",5, actual);
+    }
 
-	@Test
-	public void testSub() {
-		int a = 2;
-		int b = 3;
-		Calculator calc = new Calculator();
-		int actual = calc.sub(a, b);
-	}
+    @Test
+    public void testSub() {
+    int a = 4;
+    int b = 3;
+    Calculator calc = new Calculator();
+    int actual = calc.sub(a, b);
+    
+    Assert.assertEquals("Substraction fail",1, actual);
+    }
+    
+    @Test
+    public void testMul() {
+        int a = 4;
+        int b = 2;
+        Calculator calc = new Calculator();
+        int actual = calc.mul(a, b);
+
+        Assert.assertEquals("Mul fail", 8, actual);
+    }
+
+    @Test
+    public void testDiv() {
+        int a = 4;
+        int b = 2;
+        Calculator calc = new Calculator();
+        int actual = calc.div(a, b);
+
+        Assert.assertEquals("Div fail", 2, actual);
+    }
+
+    @Test
+    public void testIsPair() {
+        Calculator calc = new Calculator();
+
+        boolean actual = calc.isPair(1);
+        Assert.assertFalse(actual);
+
+        actual = calc.isPair(2);
+        Assert.assertTrue(actual);
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
